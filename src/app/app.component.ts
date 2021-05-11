d105dfcbd859eae5a774a96cf579a08238722171
KickAps <flo.berson06@gmail.com>
2021-05-11 08:41:26 +0200

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tuto-oc';
+    isAuth = false;
+
+    appareils = [
+        {
+          name: 'Machine à laver',
+          status: 'éteint'
+        },
+        {
+          name: 'Frigo',
+          status: 'allumé'
+        },
+        {
+          name: 'Ordinateur',
+          status: 'éteint'
+        }
+    ];
+
+
+    constructor() {
+        setTimeout(
+            () => {
+                this.isAuth = true;
+            }, 4000
+        );
+    }
+
+    onAllumer() {
+        console.log('On allume tout !');
+    }
 }
